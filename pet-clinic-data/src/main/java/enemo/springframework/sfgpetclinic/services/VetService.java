0be8b2576abddcb3c950685491bd9c7facb2033a
corsantic <aydofurkan25@gmail.com>
2018-11-21 12:44:51 +0300

    crud service for refactoring

--- pet-clinic-data/src/main/java/enemo/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/enemo/springframework/sfgpetclinic/services/VetService.java
@@ -2,14 +2,7 @@ package enemo.springframework.sfgpetclinic.services;
 
 import enemo.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet,Long> {
 
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
