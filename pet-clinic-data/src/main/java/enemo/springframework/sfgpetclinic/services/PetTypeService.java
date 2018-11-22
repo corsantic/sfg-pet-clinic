325a52306e1a22bc0ab4faa5731dd926b8e79c5c
corsantic <aydofurkan25@gmail.com>
2018-11-22 12:11:04 +0300

    pettype service

---
+++ pet-clinic-data/src/main/java/enemo/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package enemo.springframework.sfgpetclinic.services;
+
+import enemo.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+}
